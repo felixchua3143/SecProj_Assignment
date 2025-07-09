cdebb7c44599a3246988745953ce0570da40217d
felixchua3141/2/3 <132286580+felixchua3143@users.noreply.github.com>
2025-07-09 17:46:00 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+
+import cryptography
+from cryptography.fernet import Fernet
+
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
