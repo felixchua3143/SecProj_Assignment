e46e6f549b24fde9c9cf6c54f19c4ce1f96d53b5
felixchua3141/2/3 <132286580+felixchua3143@users.noreply.github.com>
2025-07-10 20:24:30 +0800

    bootstrap template

--- app.py
+++ app.py
@@ -1,31 +1,31 @@
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/index.html')
-def index():
+@app.route('/')
+def home():
     return render_template('index.html')
 
-@app.route('/shop.html')
-def shop():
-    return render_template('shop.html')
-
-@app.route('/cart.html')
+@app.route('/cart')
 def cart():
     return render_template('cart.html')
 
-@app.route('/checkout.html')
+@app.route('/checkout')
 def checkout():
     return render_template('checkout.html')
 
-@app.route('/contact.html')
+@app.route('/contact')
 def contact():
     return render_template('contact.html')
 
-@app.route('/detail.html')
+@app.route('/detail')
 def detail():
     return render_template('detail.html')
+
+@app.route('/shop')
+def shop():
+    return render_template('shop.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
