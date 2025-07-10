5ab87ec347539c63323a0ec4303774a892935119
felixchua3141/2/3 <132286580+felixchua3143@users.noreply.github.com>
2025-07-10 19:52:03 +0800

    bootstrap template

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
-from flask import Flask, render_template, request, redirect, url_for, session
-from flask_mysqldb import MySQL
-import MySQLdb.cursors
 
-from flask_bcrypt import Bcrypt
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+@app.route('/index.html')
+def index():
     return render_template('index.html')
+
+@app.route('/shop.html')
+def shop():
+    return render_template('shop.html')
+
+@app.route('/cart.html')
+def cart():
+    return render_template('cart.html')
+
+@app.route('/checkout.html')
+def checkout():
+    return render_template('checkout.html')
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/detail.html')
+def detail():
+    return render_template('detail.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
